f86cca0558a088bac9076c101e8ce8762f1657ed
Joana Areas <jo_areas@hotmail.com>
2022-10-03 22:58:16 -0300

    new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit]
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def index
     @articles = Article.all
@@ -9,4 +23,15 @@ class ArticlesController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true
-  validates :content, presence: true, length: { minimum: 6, too_short: 'Article must have at least %{count} characters.' }
+  validates :content, presence: true, length: { minimum: 6 }
 end
